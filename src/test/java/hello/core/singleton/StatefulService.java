f0f48e06894b8de42cd9e5993db773272491feb5
lby132 <quddbscjswow@naver.com>
2022-05-13 00:11:01 +0900

    싱글톤 사용시 주의 사항

--- src/test/java/hello/core/singleton/StatefulService.java
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -2,7 +2,7 @@ package hello.core.singleton;
 
 public class StatefulService {
 
-    // private int price;
+    // private int price; //싱글톤은 상태를 유지하게 설계하면 안됨
 
     public int order(String name, int price) {
         System.out.println("name = " + name + " price = " + price);
