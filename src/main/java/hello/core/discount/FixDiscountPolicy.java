e9395dc8f9844883953efe0175c485ed421c7256
lby132 <quddbscjswow@naver.com>
2022-05-17 23:36:10 +0900

    조회한 빈이 모두 필요할때, List, Map

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -3,9 +3,11 @@ package hello.core.discount;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+// @Primary 이 어노테이션이 걸려있으면 중복 빈 일때 이 어노테이션이 걸려있는 걸로 주입시켜준다. 하지만 우선순위는 @Qualifier 가 더 명확하게 써주기 때문에 @Qualifier 가 더 높다.
 @Qualifier("fixDiscountPolicy")
 public class FixDiscountPolicy implements DiscountPolicy {
 
